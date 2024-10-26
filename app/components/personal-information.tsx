f0bd05d16f7e6d24f3960b595024936d76aa4ee3
jonathanduran7 <joniduran624@gmail.com>
2024-10-26 19:27:17 -0300

    personal information

--- app/components/personal-information.tsx
+++ app/components/personal-information.tsx
@@ -1,9 +1,14 @@
-import { Formik } from "formik";
+import { Formik, FormikProps } from "formik";
 import { initialValues, personalInformationSchema } from "../schema/personal-information.schema";
+import { Box, Button } from "@mui/material";
+import { useStepperStore } from "../store/stepper.store";
 
 export default function PersonalInformation() {
+
+  const { handleNext, handleBack, activeStep } = useStepperStore();
+
   return (
-    <div className="border-red h-full" >
+    <div className="h-full" >
       Personal Infromation
       <Formik
         initialValues={initialValues}
@@ -12,63 +17,81 @@ export default function PersonalInformation() {
         }}
         validationSchema={personalInformationSchema}
       >
-        {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit} className="w-full flex flex-col items-center border border-black">
-            <div className="flex gap-7">
+        {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => {
+          return (
+            <form onSubmit={handleSubmit} className="w-full flex flex-col items-center justify-between h-full">
               <div>
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="text"
-                  name="fullName"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.fullName}
-                />
-                {errors.fullName && touched.fullName && errors.fullName && <div className="text-[#f00] text-[12px]">{errors.fullName}</div>}
+                <div className="flex gap-7 mb-4">
+                  <div>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name</label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="text"
+                      name="fullName"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.fullName}
+                    />
+                    {errors.fullName && touched.fullName && errors.fullName && <div className="text-[#f00] text-[12px]">{errors.fullName}</div>}
+                  </div>
+                  <div>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                    />
+                    {errors.email && touched.email && errors.email && <div className="text-[#f00] text-[12px]">{errors.email}</div>}
+                  </div>
+                </div>
+                <div className="flex gap-7">
+                  <div>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">Phone</label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="text"
+                      name="phone"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.phone}
+                    />
+                    {errors.phone && touched.phone && errors.phone && <div className="text-[#f00] text-[12px]">{errors.phone}</div>}
+                  </div>
+                  <div>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="country">Country</label>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="text"
+                      name="country"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.country}
+                    />
+                    {errors.country && touched.country && errors.country && <div className="text-[#f00] text-[12px]">{errors.country}</div>}
+                  </div>
+                </div>
               </div>
-              <div>
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="email"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                />
-                {errors.email && touched.email && errors.email && <div className="text-[#f00] text-[12px]">{errors.email}</div>}
-              </div>
-            </div>
-            <div className="flex gap-7">
-              <div>
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">Phone</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="text"
-                  name="phone"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.phone}
-                />
-                {errors.phone && touched.phone && errors.phone && <div className="text-[#f00] text-[12px]">{errors.phone}</div>}
-              </div>
-              <div>
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="country">Country</label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="text"
-                  name="country"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.country}
-                />
-                {errors.country && touched.country && errors.country && <div className="text-[#f00] text-[12px]">{errors.country}</div>}
-              </div>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        )}
+              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2, width: '100%' }}>
+                <Button
+                  color="inherit"
+                  sx={{ mr: 1 }}
+                  onClick={handleBack}
+                  disabled={activeStep === 0}
+                >
+                  Back
+                </Button>
+                <Box sx={{ flex: '1 1 auto' }} />
+                <Button onClick={handleNext}>
+                  Next
+                </Button>
+              </Box>
+
+            </form>
+          )
+        }}
       </Formik>
     </div>
   )
