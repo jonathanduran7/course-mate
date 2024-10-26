b9f0957ad250142c8db11eeeea385f9908136915
jonathanduran7 <joniduran624@gmail.com>
2024-10-26 20:21:42 -0300

    implement interfaces

--- app/store/form.store.tsx
+++ app/store/form.store.tsx
@@ -1,11 +1,32 @@
 import { create } from "zustand";
+import { PersonalInformation } from "../interfaces/personal-information";
+import { CourseInformation } from "../interfaces/course-information";
+
+export interface FormData {
+  personalInformation: PersonalInformation;
+  courseInformation: CourseInformation;
+}
 
 export interface FormStore {
   isFormValid: boolean;
   setFormValid: (value: boolean) => void;
+  formData: FormData;
 }
 
 export const useFormStore = create<FormStore>((set) => ({
+  formData: {
+    personalInformation: {
+      fullName: "",
+      email: "",
+      phone: "",
+      country: "",
+    },
+    courseInformation: {
+      courseName: "",
+      modality: "",
+      startDate: new Date(),
+    },
+  },
   isFormValid: true,
   setFormValid: (value) => set({ isFormValid: value }),
 }));
