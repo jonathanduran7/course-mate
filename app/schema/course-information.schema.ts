586ec5e64d122c8ce36d44fe8e2a3553ce276740
jonathanduran7 <joniduran624@gmail.com>
2024-10-26 21:00:15 -0300

    schema course information

---
+++ app/schema/course-information.schema.ts
@@ -0,0 +1,13 @@
+import * as yup from 'yup';
+
+export const initialValues = {
+  courseName: '',
+  modality: '',
+  startDate: new Date(),
+}
+
+export const courseInformationSchema = yup.object({
+  courseName: yup.string().required('El nombre del curso es requerido'),
+  modality: yup.mixed().oneOf(['presencial', 'virutal', 'hibrido'] as const).defined(),
+  startDate: yup.date().default(() => new Date()),
+});
