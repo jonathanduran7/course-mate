ca18a27d5e590024b8cf529e7a2e1588aa761fb6
jonathanduran7 <joniduran624@gmail.com>
2024-10-29 22:24:12 -0300

    add paypal information

--- app/schema/payment-method.ts
+++ app/schema/payment-method.ts
@@ -12,6 +12,9 @@ export const initialValues = {
     bankName: '',
     accountNumber: '',
   },
+  paypalInformation: {
+    email: '',
+  },
 }
 
 export const methodPaymentSchema = yup.object({
@@ -19,24 +22,29 @@ export const methodPaymentSchema = yup.object({
     .oneOf(['credit-card', 'paypal', 'bank-transfer'] as const)
     .defined()
     .required('El método de pago es requerido'),
-  cardInformation: yup.object({
-    cardName: yup.string().required('El nombre del titular de la tarjeta es requerido'),
-    cardNumber: yup.string().required('El número de la tarjeta es requerido'),
-    expirationDate: yup.string().required('La fecha de expiración es requerida'),
-    cvv: yup.string().required('El CVV es requerido'),
-  }).when('methodPayment', (methodPayment, schema) => {
-    if (methodPayment.includes('credit-card')) {
-      return schema.required('La información de la tarjeta es requerida');
-    }
-    return schema.notRequired();
+  cardInformation: yup.lazy((_, { parent }) => {
+    return parent.methodPayment === 'credit-card'
+      ? yup.object({
+        cardName: yup.string().required('El nombre del titular de la tarjeta es requerido'),
+        cardNumber: yup.string().required('El número de la tarjeta es requerido'),
+        expirationDate: yup.string().required('La fecha de expiración es requerida'),
+        cvv: yup.string().required('El CVV es requerido'),
+      })
+      : yup.mixed().notRequired();
   }),
-  bankInformation: yup.object({
-    bankName: yup.string().required('El nombre del banco es requerido'),
-    accountNumber: yup.string().required('El número de cuenta es requerido'),
-  }).when('methodPayment', (methodPayment, schema) => {
-    if (methodPayment.includes('bank-transfer')) {
-      return schema.required('La información del banco es requerida');
-    }
-    return schema.notRequired();
-  })
+  bankInformation: yup.lazy((_, { parent }) => {
+    return parent.methodPayment === 'bank-transfer'
+      ? yup.object({
+        bankName: yup.string().required('El nombre del banco es requerido'),
+        accountNumber: yup.string().required('El número de cuenta es requerido'),
+      })
+      : yup.mixed().notRequired();
+  }),
+  paypalInformation: yup.lazy((_, { parent }) => {
+    return parent.methodPayment === 'paypal'
+      ? yup.object({
+        email: yup.string().email('El email es inválido').required('El email es requerido'),
+      })
+      : yup.mixed().notRequired();
+  }),
 });
